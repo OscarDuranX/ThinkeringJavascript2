6d28f8002b2691d11b810ab27a9d76b8220e87f2
Oscar Duran <xoscarduranx@gmail.com>
2016-09-30 17:16:52 +0200

    sync

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ var person = {
 console.log(person.name);
 person.hello();
 person.bye();
+
+// The module pattern
+var personn = (function {
+    var name = "Sergi";
+    var sn1 = "Duran";
+    var sn2 = "retolor";
+
+    return{
+        hello: function () {
+            console.log("Hola " + name);
+        },
+        bye: function () {
+            console.log("Adeww " + name);
+        },
+    }
+})();
+
+personn.bye();
+
+
+
