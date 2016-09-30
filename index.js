42c9e02e8422a1a2db556287ec52c25c0abec687
Oscar Duran <xoscarduranx@gmail.com>
2016-09-30 16:20:07 +0200

    Resum

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // SCOPE : AMBIT -> ZONA del codi on una variable té vida/sentit
 // VARIABLE GLOBALS: LA PESTE / XOCS
 
+// RESUM  que no heu de fer
+//
+// var/let/const
+//
+//      $a = ""
+//
+// No fer variables globals!!
 
-function hello() {
-    $name = 'oscar';
-    console.log('Hola '+$name);
+function fire(boolea) {
+
+    var foo;
+
+    if(boolea){
+        var foo='bar';
+
+        console.log(foo);
+    }else{
+        console.log(foo);
+    }
+
 }
 
-hello();
+fire(false);
