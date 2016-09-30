2b649f6f9406b292febf370d708a6c69a9a03857
Oscar Duran <xoscarduranx@gmail.com>
2016-09-30 16:38:31 +0200

    array assosiatiu

--- index.js
+++ index.js
@@ -5,8 +5,15 @@
 // FRONTEND: Javascript
 
 // BACKEND: Object Notation: aka JSON
-var Person = {
-    'name'  : 'Oscar',
-    'sn1'   : 'Duran',
-    'age'   : '18'
-};
+var Person = [
+    {
+        'name'  : 'Oscar',
+        'sn1'   : 'Duran',
+        'age'   : '18'
+    },
+    {
+        'name'  : 'Salam',
+        'sn1'   : 'Malas',
+        'age'   : '18'
+    }
+];
